c1c2205efea7486b609ef560e029d91207150ae2
Oleksandr Shevchenko <oleksandr.shevchenko@opensynergy.com>
2018-12-18 16:29:23 +0200

    Add implementation.

--- pr_description.sh
+++ pr_description.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-echo ">>>"
+pwd
+echo ">>>>>>"
 
+git log --oneline --decorate | awk '{print $1;}' | \
+    xargs -I % sh -c \
+    " \
+    printf '* ' && git show % | sed -n '5p' | sed -e 's/^[[:space:]]*//' ; \
+    git show % | grep '    Issue:' | sed -n '1p' \
+    "
